58fd25701d6ef723071167c243923cd1a31062cb
user.email <murat_demir1985@hotmail.com>
2022-08-15 15:01:36 +0300

    Person has been defined.

--- ders1/index.js
+++ ders1/index.js
@@ -1,8 +1,8 @@
 console.log("Hello World!")
 document.getElementById("root").innerHTML = 'Merhaba';
 
-let i = 1;
-while (i<10){
+/*let i = 1;
+/*while (i<10){
 
     const element = document.createElement("div");//const=Sabit(değiştirilemez)
     element.innerHTML = i;
@@ -10,4 +10,27 @@ while (i<10){
 
     console.log(i);
     i = i +1;
+}*/
+const arr =["Ahmet","Mehmet","Can","Ela","Sude"]*/
+const arr =[
+    {
+        id:1,
+        name:"Sude Akgün",
+        age:19,
+    },
+    {
+        id:2,
+        name:"Ahmet Güneş",
+        age:25,
+    },
+    {
+        id:3,
+        name:"Ömer Dağ",
+        age:20,
+    }
+]
+for(let person of arr){
+    const element = document.createElement("div");
+    element.innerHTML=`${person.id} ${person.name} ${person.age}`;
+    document.getElementById("root").append.apply(element);
 }
